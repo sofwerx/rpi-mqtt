8ceacc53e05a13d1b075d6c5c2fceb2ebc620c9c
Ian Blenke <ian@blenke.com>
2017-10-20 19:11:34 -0400

    Add TRAVIS_BUILD_NUMBER tag for resultant image

--- push.sh
+++ push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+  docker push ${REPO}:${TRAVIS_BUILD_NUMBER}
+  docker tag ${REPO}:${TRAVIS_BUILD_NUMBER} ${REPO}:latest 
   docker push ${REPO}:latest
 fi
