794ee7ccdb3a20b0eaacec9b9252e8e13796a823
Ian Blenke <ian@blenke.com>
2017-10-20 19:13:15 -0400

    Push latest before tagging and pushing with TRAVIS_BUILD_NUMBER

--- push.sh
+++ push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push ${REPO}:${TRAVIS_BUILD_NUMBER}
-  docker tag ${REPO}:${TRAVIS_BUILD_NUMBER} ${REPO}:latest 
+  set -x
   docker push ${REPO}:latest
+  docker tag ${REPO}:latest ${REPO}:${TRAVIS_BUILD_NUMBER} 
+  docker push ${REPO}:${TRAVIS_BUILD_NUMBER}
 fi
